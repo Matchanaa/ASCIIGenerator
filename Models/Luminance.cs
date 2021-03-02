c5eab4d074f872efd8cc34a95315931bec8562ee
MattLopl <mkanumilli@gmail.com>
2021-03-02 23:27:33 +0000

    Removed unnecessary using statements

--- Models/Luminance.cs
+++ Models/Luminance.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ASCIIGenerator
+﻿namespace ASCIIGenerator
 {
   /// <summary>
   /// Constructors for the Luminance and ASCII character values, contained in the Luminances List.
